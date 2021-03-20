e7ea40843b84b80e7d0a965aa2379224668dd816
Khoi-Nguyen <78480700+Khoi-Nguyen@users.noreply.github.com>
2021-03-20 22:49:15 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,8 @@ export async function getServerSideProps(ctx) {
   const baseURL  = `https://${ctx.req.headers.host}`;
   // const items = await fetch(`https://${baseURL}/api/DAO/getLaptopList`)
   // const data = await items.json()
-  
+  const data=[]
+
   return {
     props: {
       a: `https://${baseURL}/api/DAO/getLaptopList`,
