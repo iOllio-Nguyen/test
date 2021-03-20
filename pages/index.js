f06f0640a8bf3914d6eaf2d4edf626b8e5e986af
Khoi-Nguyen <78480700+Khoi-Nguyen@users.noreply.github.com>
2021-03-20 21:33:38 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { getSession, useSession } from 'next-auth/client'
+// import { getSession, useSession } from 'next-auth/client'
 import Modal from "react-modal"
 import Link from 'next/link'
+// import axios from 'axios'
 // Styles
 import styles from '../styles/home.module.css'
 // Componet
@@ -11,76 +12,14 @@ import  SuccessLottie from "../components/utilities/SuccessLottie"
 
 
 export async function getServerSideProps(ctx) {
-  let data = ""
-  try{
-    // const items = await fetch('/api/DAO/getLaptopList')
-    // data = await items.json()
-    data=  [
-      {
-        _id: '6013eaeb30d2e9b39ded16fb',
-        name: 'Asus F570ZD',
-        cpu: 'i7-10750H',
-        ram: '8Gb 2666',
-        storage: 'SSD NVMe 512Gb M.2 PCIe Gen3x4',
-        gpu: 'GTX 1650Ti',
-        monitor: '17inch 1080P IPS 72% NTSC',
-        alias: 'gaming asus f570zd',
-        brand: 'Asus',
-        quanity: 20,
-        price: 599,
-        photo: 'https://res.cloudinary.com/dnnqp3qvg/image/upload/v1610379904/NextJS/a_12_yep4gc.jpg'
-      },
-      {
-        _id: '6017f11a7175c7d147b90010',
-        alias: 'gaming',
-        brand: 'Acer',
-        cpu: 'i5-10300H',
-        gpu: 'GTX 1660 6GB',
-        monitor: '15.6inch 1080P IPS 72% NTSC 144hz',
-        name: 'Acer Nitro5',
-        photo: 'https://res.cloudinary.com/dnnqp3qvg/image/upload/v1610379912/NextJS/a_9_mn1ouz.jpg',
-        price: 629,
-        quanity: 10,
-        ram: '8GB 2666',
-        storage: 'SSD NVMe 512GB M.2 PCIe Gen3x4'
-      },
-      {
-        _id: '6018142a7175c7d147b90011',
-        alias: 'laptop van phong',
-        brand: 'Asus',
-        cpu: 'i3-1005G1',
-        gpu: 'MX230 2GB',
-        monitor: '14inch 1080P IPS 45% NTSC',
-        name: 'Asus VivibookA504DX',
-        photo: 'https://res.cloudinary.com/dnnqp3qvg/image/upload/v1610379912/NextJS/a_9_mn1ouz.jpg',
-        price: 459,
-        quanity: 10,
-        ram: '4GB 2666',
-        storage: 'SSD NVMe 256GB M.2 PCIe Gen3x4'
-      },
-      {
-        _id: '6018147e7175c7d147b90012',
-        alias: 'vivobook asus D409DA',
-        brand: 'asus',
-        cpu: 'Ryzen5 4500U',
-        gpu: 'GTX 1650',
-        monitor: '15.6inch 1080P IPS 45% NTSC',
-        name: 'asus VivobookD409DA',
-        photo: 'https://res.cloudinary.com/dnnqp3qvg/image/upload/v1610379904/NextJS/a_12_yep4gc.jpg',
-        price: 359,
-        quanity: 15,
-        ram: '8Gb 2666',
-        storage: 'SSD NVMe 512Gb M.2 PCIe Gen3x4'
-      }
-    ]
-  }
-  catch(err){
-    data=err
-  }
-  // const userCredentical = await getSession(ctx)
+  const baseURL  = ctx.req.headers.referer;
+  const items = await fetch(`${baseURL}api/DAO/getLaptopList`)
+  const data = await items.json()
+  // const data = []
   return {
     props: {
-      data: JSON.stringify(data),
+      data,
+      baseURL: baseURL
       // userCredentical
     }
   }
@@ -89,14 +28,13 @@ export async function getServerSideProps(ctx) {
 Modal.setAppElement("#__next")
 
 const App =(props)=> {
-  console.log(props)
-  const [laptopList, setLaptopList] = useState(JSON.parse(props.data))
+  const baseURL = props.baseURL
+  const [laptopList, setLaptopList] = useState(props.data)
   const [isMessageModalOpen, setMessageModalSate] = useState(false)
 
   const fetchMoreData =async()=>{
       if(laptopList.length > 30){return}
-      
-      const items = await fetch('/api/DAO/getLaptopList')
+      const items = await fetch(`${baseURL}api/DAO/getLaptopList`)
       const data = await items.json()
       setLaptopList(laptopList.concat(data))
   }
